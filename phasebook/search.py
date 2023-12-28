dc4766cf5725ccb995a413b6cf5bc691fca552b8
Roi Rotoras <roirotoras012@gmail.com>
2023-12-28 20:02:22 +0800

    logic based on parameters for search

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,31 @@ def search_users(args):
     """
 
     # Implement search here!
-
+    if(len(args) == 1):
+        return singleParam(args)
+    elif(len(args) > 1):
+        return multipleParam(args)
     return USERS
+
+
+def singleParam(parameter):
+    returnData = USERS
+    if 'id' in parameter and parameter['id']:
+        returnData = list(filter(lambda item: item["id"] == parameter['id'], returnData))
+    elif 'name' in parameter and parameter['name']:
+        returnData = list(filter(lambda item: parameter['name'].lower() in item['name'].lower(), returnData))
+    elif 'age' in parameter and parameter['age']:
+        returnData = list(filter(lambda item: int(parameter["age"])+1 >= int(item['age']) and int(parameter["age"])-1 <= int(item['age']), returnData))
+    elif 'occupation' in parameter and parameter['occupation']:
+        returnData = list(filter(lambda item: parameter['occupation'].lower() in item['occupation'].lower(), returnData))
+    return returnData
+
+def multipleParam(parameter):
+    returnData = USERS
+    returnData = list(filter(lambda item: 
+    ("id" in parameter and item["id"] == parameter['id']) or
+    ("name" in parameter and parameter['name'].lower() in item['name'].lower()) or
+    ("age" in parameter and int(parameter["age"])+1 >= int(item['age']) and int(parameter["age"])-1 <= int(item['age'])) or
+    ("occupation" in parameter and parameter['occupation'].lower() in item['occupation'].lower())
+    , returnData))
+    return returnData
